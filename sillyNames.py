376a61c4280f0230319312b7133247fdbea14513
keydelk <keydelk@gmail.com>
2023-04-26 19:56:59 -0600

    Silly names project from Impractical Python Projects complete

---
+++ sillyNames.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  sillyNames.py
+#  
+#  Copyright 2023 Keith Keydel <keydelk@reform>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import sys, random
+
+def main(args):
+    print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+    print("A name just like Sean would pick for Gus:\n\n")
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+             "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite",
+             'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+             'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+             'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+             'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+             'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',
+             'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch',
+             'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug',
+             'Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+             "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+             'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+             'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+             "Winston 'Jazz Hands'", 'Worms')
+             
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
+
+    while True:
+        print("\n\n\033[1;31m{} {}\033[0m\n\n".format(random.choice(first), random.choice(last)))
+        try_again = input("Try again? (Press Enter else n to quit)\n")
+        if try_again.lower() == "n":
+            break
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
