f2973b6f890187d7ce0e0ce54341235f5c2a09e4
keydelk <keydelk@gmail.com>
2024-11-18 09:59:01 -0700

    getting the MNT reform up to date with everything else

--- list_cipher.py
+++ list_cipher.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 """Hides plaintext message as a null cipher in a list."""
-from random import randint
 import string
 import load_dictionary
 
-def hide_message(message, word_list):
-    """build a list using the message and word-list"""
+input_message = "meet at dawn"
+
+# strip whitespace and non-alphabetic characters
+message = ''
+for char in input_message:
+    if char in string.ascii_letters:
+        message += char
+print(message, "\n")
+
+# open dictionary file
+word_list = load_dictionary.load('groceries.txt')
+
+# build list of words for cipher
+cipher_list = []
+for letter in message:
+    for word in word_list:
+        if word[2].lower() == letter.lower() and word not in cipher_list:
+            cipher_list.append(word)
+            break
+
+if len(cipher_list) != len(message):
+    print("Word list too small, try another")
+else:
+    print("Grocery List:\n", *cipher_list, sep="\n")
